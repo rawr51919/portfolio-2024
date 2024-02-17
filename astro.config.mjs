e85fa09c40334d650a2394e79cbe03d04fad2b16
rawr51919 <colton51919@gmail.com>
2024-02-16 20:01:11 -0400

    optimized the images and projects section a little

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,13 @@ import alpinejs from "@astrojs/alpinejs";
 import robotsTxt from 'astro-robots-txt';
 import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import { DEFAULT_LOCALE, LOCALES, SITE_URL } from "./src/consts";
+// import node from "@astrojs/node"; .
 import vercel from '@astrojs/vercel/serverless';
 import vitePwa from "@vite-pwa/astro";
 import compress from "astro-compress";
 import lighthouse from "astro-lighthouse";
 import critters from "astro-critters";
+import icon from "astro-icon";
 const defaultLocale = DEFAULT_LOCALE;
 const locales = LOCALES;
 // https://astro.build/config
@@ -19,7 +21,7 @@ export default defineConfig({
   trailingSlash: "always",
   build: {
     format: "directory",
-	minify: true,
+    minify: true,
     esbuild: {
       target: 'es5' // Transpile to ES5 for broader browser support
     }
@@ -44,9 +46,13 @@ export default defineConfig({
     locales,
     defaultLocale,
     exclude: ["pages/api/**/*", "pages/rss.xml.ts", "pages/[locale]/rss.xml.ts"]
-  }), vitePwa(), compress(), lighthouse(), critters()],
+  }), vitePwa(), compress(), lighthouse(), critters(), icon()],
   output: "server",
   adapter: vercel({
 	imageService: true,
   }),
+//   adapter: node({
+//     mode: "standalone",
+//     standalone: true
+//   }),
 });
